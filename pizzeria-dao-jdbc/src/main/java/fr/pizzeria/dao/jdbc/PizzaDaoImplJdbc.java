f3c39ef7b132eb084e188b3992c3cd6d551c03d1
Franc LAVAUD <franc.lavaud@gmail.com>
2017-04-26 17:27:15 +0200

    Commit 2 Tp 1 DB done

--- pizzeria-dao-jdbc/src/main/java/fr/pizzeria/dao/jdbc/PizzaDaoImplJdbc.java
+++ pizzeria-dao-jdbc/src/main/java/fr/pizzeria/dao/jdbc/PizzaDaoImplJdbc.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import fr.pizzeria.dao.IPizzaDao;
 import fr.pizzeria.dao.exception.CreditException;
@@ -21,35 +22,67 @@ import fr.pizzeria.model.CategoriePizza;;
 
 public class PizzaDaoImplJdbc implements IPizzaDao{
 
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} 
+		catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private Connection myConnection;
+	private Statement statement;
+	
+	
+	
+	private void connectDB(){
+		
+		ResourceBundle bundle=ResourceBundle.getBundle("jdbc");
+		String urlDB=bundle.getString("urlDB.val");
+		String userDB=bundle.getString("userDB.val");
+		String mdpDB=bundle.getString("mdpDB.val");
+		
+		try {
+			myConnection = DriverManager.getConnection(urlDB,userDB,mdpDB);
+			myConnection.setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			statement=myConnection.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	private void closeDB(){
+		
+		try{
+
+			statement.close();
+			myConnection.close();
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+		
 	@Override
 	public List<Pizza> findAllPizzas() {
 		
 		List<Pizza> listPizza=new ArrayList<>();
 		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection myConnection=null;
-		try {
-			myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzeria?useSSL=false","root","");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		Statement statementQuery=null;
-		
-		try {
-			statementQuery=myConnection.createStatement();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		this.connectDB();
 		
 		ResultSet resultat=null;
 		
 		try {
-			resultat=statementQuery.executeQuery("SELECT * FROM pizza");
+			resultat=statement.executeQuery("SELECT * FROM pizza");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -67,31 +100,130 @@ public class PizzaDaoImplJdbc implements IPizzaDao{
 			e.printStackTrace();
 		}
 		
+
+		this.closeDB();
+		
+		
 		return listPizza;
 	}
 
 	@Override
 	public List<Client> findAllClient() {
 		
-		return null;
+		List<Client> listClient=new ArrayList<>();
+		
+		this.connectDB();
+		
+		ResultSet resultat=null;
+		
+		try {
+			resultat=statement.executeQuery("SELECT * FROM client");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			while(resultat.next()){ 
+				 Client client=new Client(resultat.getInt("ID"),
+						 resultat.getString("Nom"),
+						 resultat.getString("Prenom"),
+						 resultat.getDouble("Solde"));
+				 listClient.add(client);
+				 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		this.closeDB();
+		
+		return listClient;
+		
 	}
 
 	@Override
 	public boolean saveNewPizza(Pizza pizza) throws SavePizzaException {
 		
+		this.connectDB();
+		
+		int flag=0;
+		String requete="INSERT INTO `pizza`(`Code`, `Name`, `Prix`, `Categorie`) "
+				+ "VALUES ('"+pizza.getCode()+"','"+pizza.getNom()+"','"+pizza.getPrix()+"','"+pizza.getCategorie()+"')";
+		try {
+			flag=statement.executeUpdate(requete);
+			myConnection.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try{myConnection.rollback();}catch(SQLException x){x.printStackTrace();}
+		}
+		
+		
+		this.closeDB();
+		
+		if(flag==0){
 		return false;
+		}
+		
+		else{
+			return true;
+		}
 	}
 
 	@Override
 	public boolean updatePizza(String codePizza, Pizza pizza) throws UpdatePizzaException {
 		
+		this.connectDB();
+		
+		int flag=0;
+		String requete="UPDATE `pizza` SET "
+				+ "`Code`='"+pizza.getCode()+"',"
+				+ "`Name`='"+pizza.getNom()+"',"
+				+ "`Prix`='"+pizza.getPrix()+"',"
+				+ "`Categorie`='"+pizza.getCategorie()+"' "
+				+ "WHERE Code='"+codePizza+"';";
+		try {
+			flag=statement.executeUpdate(requete);
+			myConnection.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try{myConnection.rollback();}catch(SQLException x){x.printStackTrace();}
+		}
+		
+		this.closeDB();
+		
+		if(flag==0){
 		return false;
+		}
+		
+		else{
+			return true;
+		}
+		
 	}
 
 	@Override
 	public boolean deletePizza(String codePizza) throws DeletePizzaException {
+		this.connectDB();
 		
-		return false;
+		int flag=0;
+		String requete="DELETE FROM `pizza` WHERE Code='"+codePizza+"';";
+		try {
+			flag=statement.executeUpdate(requete);
+			myConnection.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try{myConnection.rollback();}catch(SQLException x){x.printStackTrace();}
+		}
+		
+		this.closeDB();
+		
+		if(flag==0){
+			return false;
+		}
+			
+		else{
+			return true;
+		}
 	}
 
 	@Override
